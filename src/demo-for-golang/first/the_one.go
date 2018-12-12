db302d45fc772cb8a6281425bd64bceb797eee21
zhang-boyang <zhangby_fx@foxmail.com>
2018-12-13 00:31:34 +0800

    first episode, build evn

---
+++ src/demo-for-golang/first/the_one.go
@@ -0,0 +1,9 @@
+package first
+
+import (
+	"fmt"
+)
+
+func PrintFirst() {
+	fmt.Print("This is first episode, build enviroment\n")
+}
---
+++ src/demo-for-golang/main/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"demo-for-golang/first"
+)
+
+func main() {
+	first.PrintFirst()
+}
